5fc2c9b999ff5969de67eb7e9936c4881bccda46
nakanodesukun <94818124+nakanodesukun@users.noreply.github.com>
2021-12-29 12:07:57 +0900

    Initial Commit

---
+++ idealKadai6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  idealKadai6
+//
+//  Created by 中野翔太 on 2021/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+enum 
+
+}
+
